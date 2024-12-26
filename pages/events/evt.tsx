2170adfd020b43c1fd084ac9ca1cb387fc307909
jayCde <jeff.k.asmah@outlook.com>
2024-12-26 16:57:55 +0000

    misc: add pages

---
+++ pages/events/evt.tsx
@@ -0,0 +1,10 @@
+import EventDetail from "@/components/events/eventDetails";
+import PageWrapper from "@/components/globals/wrapper";
+
+export default function EventDetailsPage() {
+  return (
+    <PageWrapper>
+      <EventDetail />
+    </PageWrapper>
+  );
+}
