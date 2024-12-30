195badd1b75d3b62fa430df4b0710e17ff8b350d
Emmanuel Baah <emmanuelhaab14@gmail.com>
2024-12-30 12:41:12 +0000

    Fixed `gap` utility causing layout misalignment

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,9 @@ export default function Home() {
       <div className="ml-3  grid grid-cols-2 gap-4  w-screen h-screen justify-evenly items-center">
         <div className="grid grid-row-3 gap-4">
           <div className="flex items-center ">
-            <div className="  text-white  grid grid-cols-3 gap-3 justify-items-center ">
-              <div className="flex items-center justify-center bg-black w-48 h-32 rounded-md">Ads</div>
-              <div className="flex items-center justify-center bg-yellow-500 w-48 h-32 rounded-md">Events</div>
+            <div className="  text-white  flex-grow row-content-between gap-5 justify-items-center ">
+              <div className="flex items-center justify-center bg-black w-80 h-32 rounded-md">Ads</div>
+              <div className="flex items-center justify-center bg-yellow-500 w-72 h-32 rounded-md">Events</div>
             </div>
           </div>
           <div className="h-72 bg-red-800 text-white flex items-center justify-center rounded-md">
@@ -32,9 +32,9 @@ export default function Home() {
             Baptism
           </div>
           <div className="flex items-center ">
-            <div className=" text-white justify-items-center grid grid-cols-3 gap-3 rounded-md">
-              <div className="flex items-center justify-center bg-purple-800 w-48 h-32 rounded-md">The Yout</div>
-              <div className="flex items-center justify-center bg-purple-800 w-48 h-32 rounded-md ">Sceva </div>
+            <div className=" text-white justify-items-center gap-5 flex-grow row-content-between">
+              <div className="flex items-center justify-center bg-purple-800 w-72 h-32 rounded-md">The Yout</div>
+              <div className="flex items-center justify-center bg-purple-800 w-72 h-32 rounded-md ">Sceva </div>
             </div>
           </div>
         </div>
